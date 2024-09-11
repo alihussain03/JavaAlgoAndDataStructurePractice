8e14cce1ad6951a12a42dfc0a0d432274b29ae44
alihussain03 <alihussain.personal3@gmail.com>
2024-09-11 10:41:22 +0300

    Add Array_10_RotateArray

--- src/com/practice/Array/Array_11_MoveZerosToEnd.java
+++ src/com/practice/Array/Array_11_MoveZerosToEnd.java
@@ -1,6 +1,6 @@
 package com.practice.Array;
 
-public class MoveZerosToEnd {
+public class Array_11_MoveZerosToEnd {
     public static void main(String[] args) {
         int[] arr = {4, 0, 6, 7, 0, 1, 0, 6}; // Output: [4, 6, 7, 1, 6, 0, 0, 0]
         moveZerosToEnd(arr);
@@ -31,19 +31,21 @@ public class MoveZerosToEnd {
         int slow = 0;
         for (int i : arr) {
             if (i == 0) {
-                slow = 1;
+                slow = i;
                 break;
             }
         }
+
+        // Start the fast pointer from slow + 1 and move non-zero elements
         for (int fast = slow + 1; fast < arr.length; fast++) {
             if (arr[fast] != 0) {
+                // Swap non-zero element at fast with the zero at slow
                 int temp = arr[slow];
                 arr[slow] = arr[fast];
                 arr[fast] = temp;
-                slow++;
+                slow++; // Move slow to the next position where zero is expected
             }
         }
-
         for (int num : arr) {
             System.out.print(num + " ");
         }
