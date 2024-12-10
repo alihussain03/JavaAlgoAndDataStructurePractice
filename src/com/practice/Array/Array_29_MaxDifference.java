df0d48aa99ce66e2d7a78b747d1569ba89303e8e
Ali Hussain <alihussain.personal3@gmail.com>
2024-12-10 09:53:11 +0200

    Max Difference

---
+++ src/com/practice/Array/Array_29_MaxDifference.java
@@ -0,0 +1,21 @@
+package com.practice.Array;
+
+/* https://leetcode.com/problems/maximum-difference-between-increasing-elements/ */
+public class Array_29_MaxDifference {
+    public static void main(String[] args) {
+        int nums[] = new int[]{1, 5, 2, 10};
+        System.out.println(maximumDifference(nums));
+    }
+
+    static int maximumDifference(int[] nums) {
+        int min = nums[0];
+        int maxDifference = -1;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > min) {
+                maxDifference = Math.max(maxDifference, nums[i] - min);
+            } else min = nums[i];
+        }
+        return maxDifference;
+    }
+}
