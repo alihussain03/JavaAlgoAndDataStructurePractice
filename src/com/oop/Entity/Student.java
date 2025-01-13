9711e8d0427933eee2b6bce0d58ff1f5fc731c53
Ali Hussain <alihussain.personal3@gmail.com>
2025-01-13 20:23:50 +0200

    OOP Practice

---
+++ src/com/oop/Entity/Student.java
@@ -0,0 +1,39 @@
+package com.oop.Entity;
+
+public class Student extends Person {
+    int rollNo;
+    String course;
+    private static int fee = 1000;
+
+    public Student(String name, int age, int rollNo, String course) {
+        super(name, age);
+        this.rollNo = rollNo;
+        this.course = course;
+    }
+
+    public int getRollNo() {
+        return rollNo;
+    }
+
+    public void setRollNo(int rollNo) {
+        this.rollNo = rollNo;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+    @Override
+    public void introduce() {
+        super.introduce();
+        System.out.println("I am studying " + course + " and my roll number is " + rollNo + ".");
+    }
+
+    public void study() {
+        System.out.println("I am studying.");
+    }
+}
