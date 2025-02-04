4d06d7d4014de18dad3049a9665058c377c93346
Ali Hussain <alihussain.personal3@gmail.com>
2025-02-04 09:14:23 +0200

    rearrange array programs

---
+++ src/Grokking/Arrray/ContainsDuplicates.java
@@ -0,0 +1,21 @@
+package Grokking.Arrray;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ContainsDuplicates {
+    public static void main(String[] args) {
+        int[] nums = {7, 1, 4};
+        System.out.println("----------\nArray has duplicate: " + checkDuplicateElementExistInArrayUsingHashSet(nums));
+    }
+
+    static boolean checkDuplicateElementExistInArrayUsingHashSet(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (!set.add(num)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
