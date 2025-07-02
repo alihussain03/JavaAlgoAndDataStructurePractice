cc3df45da621b0d706a328fcd2f5f71b98e01109
Ali Hussain <alihussain.personal3@gmail.com>
2025-07-03 02:37:59 +0300

    Add string examples

--- src/AlgoMaster/HashTables/ContainsDuplicate.java
+++ src/AlgoMaster/HashTables/ContainsDuplicate.java
@@ -1,4 +1,33 @@
 package AlgoMaster.HashTables;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class ContainsDuplicate {
+
+  public static void main(String[] args) {
+    int[] arr = {1,2,3,1,2,3};
+    System.out.println(containsNearbyDuplicate(arr, 2));
+  }
+
+  static boolean containsNearbyDuplicate(int[] nums, int k) {
+    Set<Integer> window = new HashSet<>();
+
+    for (int i = 0; i < nums.length; i++) {
+      // If the current number is already in the set, we found a duplicate within range
+      if (window.contains(nums[i])) {
+        return true;
+      }
+
+      // Add the current number to the set
+      window.add(nums[i]);
+
+      // Maintain the window size to be at most k
+      if (window.size() > k) {
+        window.remove(nums[i - k]); // Remove the element that is out of the window
+      }
+    }
+
+    return false; // No duplicates found within k distance
+  }
 }
--- src/AlgoMaster/HashTables/MaximumBalloons.java
+++ src/AlgoMaster/HashTables/MaximumBalloons.java
@@ -2,6 +2,7 @@ package AlgoMaster.HashTables;
 
 import java.util.HashMap;
 import java.util.Map;
+import util.GeneralUtil;
 
 /* https://leetcode.com/problems/maximum-number-of-balloons/description/
  * https://www.youtube.com/watch?v=fyST-_s97QU
@@ -10,18 +11,53 @@ public class MaximumBalloons {
 
   public static void main(String[] args) {
     System.out.println("Max Ballons are: " + maxNumberOfBalloons("loonbalxballpoon"));
+
+    GeneralUtil.newFunctionCall();
+    System.out.println("Max Ballons are: " + maxNumberOfBalloonsSpeedEfficient("loonbalxballpoon"));
   }
 
   static int maxNumberOfBalloons(String text) {
-    Map<Character, Integer> count = new HashMap<>();
+    Map<Character, Integer> characterCount = new HashMap<Character, Integer>();
     for (char c : text.toCharArray()) {
-      if (c == 'b' || c == 'a' || c == 'l' || c == 'o' || c == 'n') {
-        count.put(c, count.getOrDefault(c, 0) + 1);
+      characterCount.put(c, characterCount.getOrDefault(c, 0) + ("balon".indexOf(c) >= 0 ? 1 : 0));
+    }
+
+    int numberOnce = Math.min(characterCount.getOrDefault('b', 0),
+        Math.min(characterCount.getOrDefault('a', 0), characterCount.getOrDefault('n', 0)));
+
+    int numberTwice = Math.min(characterCount.getOrDefault('l', 0), characterCount.getOrDefault('o', 0));
+
+    return Math.min(numberTwice / 2, numberOnce);
+  }
+
+  static int maxNumberOfBalloonsSpeedEfficient(String text) {
+    int b = 0, a = 0, l = 0, o = 0, n = 0;
+
+    for (char ch : text.toCharArray()) {
+      switch (ch) {
+        case 'b':
+          b++;
+          break;
+        case 'a':
+          a++;
+          break;
+        case 'l':
+          l++;
+          break;
+        case 'o':
+          o++;
+          break;
+        case 'n':
+          n++;
+          break;
       }
     }
-    int x = Math.min(count.getOrDefault('b', 0), Math.min(count.getOrDefault('a', 0), count.getOrDefault('n', 0)));
-    int xx = Math.min(count.getOrDefault('l', 0), count.getOrDefault('o', 0));
 
-    return Math.min(xx / 2, x);
+    // 'l' and 'o' appear twice in "balloon"
+    l /= 2;
+    o /= 2;
+
+    // return minimum of all counts
+    return Math.min(Math.min(Math.min(b, a), Math.min(l, o)), n);
   }
 }
