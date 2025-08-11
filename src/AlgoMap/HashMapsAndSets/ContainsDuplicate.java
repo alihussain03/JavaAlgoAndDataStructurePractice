8771b5f32decbab1ec714a200b564d305fd58f52
Ali Hussain <alihussain.personal3@gmail.com>
2025-08-11 21:19:04 +0300

    HashTables example

--- src/AlgoMap/HashMapsAndSets/ContainsDuplicate.java
+++ src/AlgoMap/HashMapsAndSets/ContainsDuplicate.java
@@ -1,5 +1,25 @@
 package AlgoMap.HashMapsAndSets;
 
+import java.util.HashMap;
+
+/**
+ * <a href="https://leetcode.com/problems/contains-duplicate/description/">LeetCode</a>
+ */
 public class ContainsDuplicate {
 
+  public static void main(String[] args) {
+    System.out.println(containsDuplicate(new int[]{1, 2, 3, 1}));
+  }
+
+  static boolean containsDuplicate(int[] nums) {
+    HashMap<Integer, Integer> mapST = new HashMap<>();
+    for (int num : nums) {
+      if (!mapST.containsKey(num)) {
+        mapST.put(num, 1);
+      } else {
+        return true;
+      }
+    }
+    return false;
+  }
 }
--- src/AlgoMap/HashMapsAndSets/JewelsAndStones.java
+++ src/AlgoMap/HashMapsAndSets/JewelsAndStones.java
@@ -1,5 +1,32 @@
 package AlgoMap.HashMapsAndSets;
 
+import java.util.HashSet;
+import java.util.Set;
+import util.GeneralUtil;
+
+/**
+ * <a href="https://leetcode.com/problems/jewels-and-stones/description/">LeetCode</a>
+ */
 public class JewelsAndStones {
 
+  public static void main(String[] args) {
+    System.out.println(numJewelsInStones("aA", "aAAbbbb"));
+    GeneralUtil.newFunctionCall();
+    System.out.println(numJewelsInStones("z", "ZZ"));
+    GeneralUtil.newFunctionCall();
+  }
+
+  static int numJewelsInStones(String jewels, String stones) {
+    Set<Character> jewelsSet = new HashSet<>();
+    for (char c : jewels.toCharArray()) {
+      jewelsSet.add(c);
+    }
+    int sum = 0;
+    for (char c : stones.toCharArray()) {
+      if (jewelsSet.contains(c)) {
+        sum++;
+      }
+    }
+    return sum;
+  }
 }
